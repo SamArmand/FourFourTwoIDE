af70d4a2fd56ccb4926b2da123658b53ffd02632
Sam Assaf <sam.assaf@icloud.com>
2016-02-28 11:57:05 -0500

    hopefully this works

--- src/compiler/Token.java
+++ src/compiler/Token.java
@@ -1,18 +1,20 @@
 package compiler;
 
+import java.util.Arrays;
+
 //Data structure for storing information about a token
 public class Token {
 
 	private String type;
 	private String lexeme;
-	private int lineNum;
+	private int line;
 	
 	//Constructor
-	public Token(String type, String lexeme, int lineNum) {
+	public Token(String type, String lexeme, int line) {
 		
 		this.type = type;
 		this.lexeme = lexeme;
-		this.lineNum = lineNum;
+		this.line = line;
 		
 	}
 
@@ -20,11 +22,22 @@ public class Token {
 		return type;
 	}
 
+	public int getLine() {
+		return line;
+	}
+
 	//returns formatted Token info
 	public String toString() {
+
+		return String.format("TOKEN | Type: %-15s Lexeme: %-15s Line: %-5s", type, lexeme, line);
 		
-		return String.format("TOKEN | Type: %-8s Lexeme: %-15s Line: %-5s", type, lexeme, lineNum);
-		
+	}
+
+	//checks if token belongs to set
+	public boolean belongsTo(String[] set) {
+
+		return Arrays.asList(set).contains(type);
+
 	}
 	
 }
