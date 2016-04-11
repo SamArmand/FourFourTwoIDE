bf0932b3807c9b21e536b552f91cb84e41b18a86
JessicaFalco1 <j.falco93@gmail.com>
2016-04-10 22:47:15 -0400

    comments

--- src/ui/Outputter.java
+++ src/ui/Outputter.java
@@ -1,10 +1,11 @@
 package ui;
 
+// Static class that holds the strings that will be part of each output
 public class Outputter {
 
-    public static StringBuilder tokenStrings;
-    public static StringBuilder errorStrings;
-    public static StringBuilder derivationStrings;
-    public static StringBuilder symbolTablesStrings;
+    public static StringBuilder tokenStrings;           // All lexical tokens saved
+    public static StringBuilder errorStrings;           // All errors (Tokens, syntac and  sematic errors)
+    public static StringBuilder derivationStrings;      // All grammar productions
+    public static StringBuilder symbolTablesStrings;    // All symbol tables
 
 }
